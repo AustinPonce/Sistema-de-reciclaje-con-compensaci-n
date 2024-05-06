1fd2ba881912ab90a39cbc9f8b7d7205d99bf2f2
AustinPonce <167488463+AustinPonce@users.noreply.github.com>
2024-05-06 12:17:09 -0600

    Panel

--- Controlador/Panel1.java
+++ Controlador/Panel1.java
@@ -1,8 +1,10 @@
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -14,17 +16,16 @@ import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 @SuppressWarnings("serial")
-public class Panel1 extends JFrame {
+public class Panel1 extends JFrame implements Serializable {
 
 	private JPanel Principal;
-	ArrayList <Resiclaje> U = new ArrayList <Resiclaje>();
-	//Componentes de la interfaz de usuario
-	
-	
+	ArrayList<Resiclaje> U = new ArrayList<Resiclaje>();
+	// Componentes de la interfaz de usuario
+
 	public Panel1() {
-		
+
 		setTitle("Sistema de resiclaje ");
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		Principal = new JPanel();
@@ -32,86 +33,118 @@ public class Panel1 extends JFrame {
 		Principal.setToolTipText("Registro de usuarios");
 		Principal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(Principal);
-		
-		JLabel lblBienvenidosAlRegistro = new JLabel("Bienvenidos al sistema de resiclaje"
-				+ "");
+
+		JLabel lblBienvenidosAlRegistro = new JLabel("Bienvenidos al sistema de resiclaje" + "");
 		lblBienvenidosAlRegistro.setBounds(100, 11, 350, 23);
 		lblBienvenidosAlRegistro.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		Principal.add(lblBienvenidosAlRegistro);
-		
+
 		JLabel label = new JLabel("");
 		label.setBounds(527, 331, 137, 164);
 		Principal.add(label);
-		
 
-		JButton btnIngresar = new JButton("Ingresar datos");
-		btnIngresar.setBounds(42, 59, 138, 23);
-		btnIngresar.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		
-		btnIngresar.addActionListener(new ActionListener() {
+		/****************************************************************************************************************************************************************/
+		// Botón para ingresar datos
+		JButton btnPapel = new JButton("Reciclaje de Papel");
+		btnPapel.setBounds(42, 59, 138, 23);
+		btnPapel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		btnPapel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				Resiclaje	Informacion = new Resiclaje();
-				U.add(Informacion);			
+				Papel Informacion = new Papel();
+				U.add(Informacion);
 			}
-		});		
+		});
 		Principal.setLayout(null);
 
-		btnIngresar.setToolTipText("Ingresar datos de usuarios");
-		Principal.add(btnIngresar);
-		
+		btnPapel.setToolTipText("Ingresar datos de usuarios");
+		Principal.add(btnPapel);
+
+		// Botón para ingresar datos
+		JButton btnPlastico = new JButton("Reciclaje de Plastico");
+		btnPlastico.setBounds(261, 59, 138, 23);
+		btnPlastico.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		btnPlastico.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Plastico Informacion = new Plastico();
+				U.add(Informacion);
+			}
+		});
+		Principal.setLayout(null);
+
+		btnPlastico.setToolTipText("Ingresar datos de usuarios");
+		Principal.add(btnPlastico);
+
+		// Botón para ingresar datos
+		JButton btnVidrio = new JButton("Reciclaje de vidrio");
+		btnVidrio.setBounds(42, 104, 138, 23);
+		btnVidrio.setFont(new Font("Tahoma", Font.PLAIN, 10));
+
+		btnVidrio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Vidrio Informacion = new Vidrio();
+				U.add(Informacion);
+			}
+		});
+		Principal.setLayout(null);
+
+		btnVidrio.setToolTipText("Ingresar datos de usuarios");
+		Principal.add(btnVidrio);
 
 		// Botón para mostrar datos
 		JButton btnMostrarDatos = new JButton("Mostrar datos");
 		btnMostrarDatos.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnMostrarDatos.setBounds(261, 59, 138, 23);
+		btnMostrarDatos.setBounds(261, 104, 138, 23);
 		btnMostrarDatos.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				Mostrar();
 			}
 		});
-		
+
 		btnMostrarDatos.setToolTipText("Muestra datos del usuario");
 		Principal.add(btnMostrarDatos);
-		
+
 		// Botón para eliminar datos
-		JButton btnEliminarDatos =  new JButton("Eliminar datos");
-		btnEliminarDatos.setBounds(42, 104, 138, 23);
+		JButton btnEliminarDatos = new JButton("Eliminar datos");
+		btnEliminarDatos.setBounds(42, 153, 138, 23);
 		btnEliminarDatos.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnEliminarDatos.setToolTipText("Elimina datos de usuarios");
 		btnEliminarDatos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-					Eliminar();
+				Eliminar();
 			}
 		});
 		Principal.add(btnEliminarDatos);
 		// Botón para salir
 		JButton btnSalir = new JButton("Salir");
-		btnSalir.setBounds(261, 104, 138, 23);;
+		btnSalir.setBounds(261, 153, 138, 23);
+		;
 		btnSalir.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnSalir.addActionListener(new ActionListener() {
-		
+
 			public void actionPerformed(ActionEvent e) {
-				
-				int a = JOptionPane.showConfirmDialog(null,"Esta seguro de que desea salir del registro?", "Confirmaci�n",JOptionPane.YES_NO_OPTION);
-				if(JOptionPane.YES_NO_OPTION == a) {
-					JOptionPane.showMessageDialog(null, "Gracias por preferirnos", "Cerrando sistema", JOptionPane.INFORMATION_MESSAGE);
+
+				int a = JOptionPane.showConfirmDialog(null, "Esta seguro de que desea salir del registro?",
+						"Confirmaci�n", JOptionPane.YES_NO_OPTION);
+				if (JOptionPane.YES_NO_OPTION == a) {
+					JOptionPane.showMessageDialog(null, "Gracias por preferirnos", "Cerrando sistema",
+							JOptionPane.INFORMATION_MESSAGE);
 					System.exit(0);
-				}	
+				}
 			}
-				
+
 		});
-		
+
 		btnSalir.setToolTipText("Salida del registro");
 		Principal.add(btnSalir);
-		
+
 	}
-	
-	 // Método para mostrar los datos almacenados en el ArrayList U
+
+	// Método para mostrar los datos almacenados en el ArrayList U
 	protected void Mostrar() {
-		
 		JFrame frame = new JFrame();
 		TextArea textArea = new TextArea (30,70);
 		textArea.setEditable(false);
@@ -138,47 +171,51 @@ public class Panel1 extends JFrame {
 
 		else {
 		JOptionPane.showMessageDialog(null, "No hay datos que se puedan mostrar", "No tenemos ningun registro", JOptionPane.INFORMATION_MESSAGE);
+	}}
+
+	// fin mostrar
+	// Método para eliminar datos de un usuario
+	public void Eliminar() {
+		// Permite al usuario eliminar los datos de un usuario específico
+		@SuppressWarnings("unused")
+		JFrame frame = new JFrame();
+		TextArea textArea = new TextArea(30, 70);
+		textArea.setEditable(false);
+		@SuppressWarnings("unused")
+		JScrollPane A = new JScrollPane(textArea);
+		boolean v = false;
+		int c;
+		String Datos = "";
+
+		c = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el ID del usuario que desea eliminar",
+				"Eliminaci�n de usuario", JOptionPane.INFORMATION_MESSAGE));
+
+		for (int Movimiento = 0; Movimiento < U.size(); Movimiento++) {
+
+			if (U.get(Movimiento).getId() == c) {
+				v = true;
+				int b = JOptionPane.showConfirmDialog(null, "Desea confirmar la Eliminacion", "Eliminamos?",
+						JOptionPane.YES_NO_OPTION);
+				if (b == JOptionPane.YES_OPTION) {
+					U.remove(Movimiento);
+					JOptionPane.showMessageDialog(null, "Eliminacion Exitosa.", "Eliminacion Realizada",
+							JOptionPane.INFORMATION_MESSAGE);
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Eliminacion Cancelada.", "Eliminacion Cancelada",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+
+			for (int i = 0; i < U.size(); i++) {
+				Datos += (i + 1) + "" + U.get(i);
+			}
+			textArea.setText(Datos);
+		}
+		if (!v) {
+			JOptionPane.showMessageDialog(null, "Registro no encontrado.", "Notificacion",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
-		
-		
-	}//fin mostrar
-  // Método para eliminar datos de un usuario
-  public void Eliminar() {
-    // Permite al usuario eliminar los datos de un usuario específico	
-    @SuppressWarnings("unused")
-    JFrame frame = new JFrame();
-    TextArea textArea = new TextArea (30,70);
-    textArea.setEditable(false);
-    @SuppressWarnings("unused")
-    JScrollPane A = new JScrollPane(textArea);
-    boolean v =false;
-    int c;
-    String Datos = "";	
-    
-         c = Integer.parseInt( JOptionPane.showInputDialog(null, "Ingrese el ID del usuario que desea eliminar","Eliminaci�n de usuario",JOptionPane.INFORMATION_MESSAGE));	
 
-         for(int Movimiento = 0; Movimiento < U.size(); Movimiento++) {
-                
-            if(U.get(Movimiento).getId()== c){
-             v=true;
-        int b =JOptionPane.showConfirmDialog(null, "Desea confirmar la Eliminacion","Eliminamos?", JOptionPane.YES_NO_OPTION);
-                if(b==JOptionPane.YES_OPTION) {
-        U.remove(Movimiento);
-        JOptionPane.showMessageDialog(null, "Eliminacion Exitosa.", "Eliminacion Realizada",JOptionPane.INFORMATION_MESSAGE);
-
-                }else {
-                    JOptionPane.showMessageDialog(null, "Eliminacion Cancelada.", "Eliminacion Cancelada",JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-            
-            
-        for(int i = 0; i < U.size(); i ++) {
-            Datos += (i+1)+""+ U.get(i);
-        }
-            textArea.setText(Datos);
-        }
-        if (!v) {
-                JOptionPane.showMessageDialog(null,"Registro no encontrado.", "Notificacion",JOptionPane.INFORMATION_MESSAGE);
-        }
 }
-	}
