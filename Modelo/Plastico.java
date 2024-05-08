25de6820b9686366012060e84735de08d6753ab4
AustinPonce <167488463+AustinPonce@users.noreply.github.com>
2024-05-07 21:41:11 -0600

    plastico

--- Modelo/Plastico.java
+++ Modelo/Plastico.java
@@ -248,7 +248,7 @@ public class Plastico extends Resiclaje {// star class
 			public void actionPerformed(ActionEvent e) {
 
 				int a = JOptionPane.showConfirmDialog(null, "Estás seguro de que quieres salir?",
-						"Salida de ingreso de datos", JOptionPane.YES_NO_OPTION);
+						"Ingrese la informacion solicitada", JOptionPane.YES_NO_OPTION);
 
 				if (JOptionPane.YES_NO_OPTION == a) {
 					setName("Nulo");
